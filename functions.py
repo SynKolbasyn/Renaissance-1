c8817e344eb5bef51905f52309948c7d6eb134b7
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-06-03 00:01:35 +0300

    Added events which player can do

--- functions.py
+++ functions.py
@@ -15,3 +15,11 @@ def create_new_account(name: str, login: str, identification_number: int):
     player_data_base[f"{identification_number}"] = player.Player(name, login, identification_number).json()
     with open("players_data_base.json", "w") as file:
         file.write(ujson.dumps(player_data_base, indent=2))
+
+
+def get_players_events(identification_number: int) -> list:
+    with open("players_data_base.json", "r") as file:
+        player_data_base = ujson.load(file)
+    with open("locations.json", "r") as file:
+        locations = ujson.load(file)
+    return locations[player_data_base[f"{identification_number}"]["location"]]["events"]
--- main.py
+++ main.py
@@ -14,9 +14,12 @@ dp = aiogram.Dispatcher(bot)
 async def start(message: aiogram.types.Message):
     if not functions.is_account_exist(message.from_user.id):
         functions.create_new_account(message.from_user.first_name, message.from_user.username, message.from_user.id)
+    keyboard = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*functions.get_players_events(message.from_user.id))
     await message.answer(f"Hello {message.from_user.first_name}. "
                          f"Welcome to Renaissance, here you can do whatever you want, "
-                         f"and your path is determined only by you. What are you waiting for, let's go!")
+                         f"and your path is determined only by you. What are you waiting for, let's go!",
+                         reply_markup=keyboard)
 
 
 if __name__ == "__main__":
